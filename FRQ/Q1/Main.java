a5f232b02cdfe37ec53e84e477d4f2e5206e750a
LazareSchool <lazarec@nycstudents.net>
2024-05-22 09:47:27 -0400

    Create Main.java

---
+++ FRQ/Q1/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    Feeder feeder = new Feeder();
+    feeder.simulateManyDays(1,9);
+  }
+
+}
